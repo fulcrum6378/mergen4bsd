c42e808491e552fbfd35f5c86e0dc7d6d23f6ce0
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-11-24 23:14:56 +0000

    2023.11.24 - 2 (Speaker partially implemented)

--- aud/speaker.cpp
+++ aud/speaker.cpp
@@ -1,21 +1,51 @@
-#include <fcntl.h> // O_RDWR (oflag)
+#include <fcntl.h> // O_WRONLY (oflag)
 #include <iostream> // perror
 #include <sys/ioctl.h>
 #include <sys/soundcard.h> // Open Sound System
 #include <unistd.h> // close
 
-#include "../global.hpp"
 #include "speaker.hpp"
 
 Speaker::Speaker(int *exit) {
-    if ((dev = open("/dev/dsp0.0", O_RDWR)) == -1) {
+    // open device
+    if ((dev = open("/dev/dsp0.0", O_WRONLY)) == -1) {
         perror("Failed to open device");
         *exit = 1;
         return;
     }
-    int ver;
-    ioctl(dev, OSS_GETVERSION, &ver);
-    print("OSS Version: %d", ver);
+
+    // set audio format
+    int tmp = AUD_OUT_FORMAT;
+    if (ioctl(dev, SNDCTL_DSP_SETFMT, &tmp) == -1) {
+        perror("Couldn't set audio format");
+        *exit = 2;
+    }
+    if (tmp != AUD_OUT_FORMAT) {
+        perror("This device doesn't support this audio format");
+        *exit = 3;
+    }
+
+    // set audio channels
+    tmp = AUD_OUT_CHANNELS;
+    if (ioctl(dev, SNDCTL_DSP_CHANNELS, &tmp) == -1) {
+        perror("Couldn't set audio channels");
+        *exit = 4;
+    }
+    if (tmp != AUD_OUT_CHANNELS) {
+        perror("This device doesn't support this number of channels");
+        *exit = 5;
+    }
+
+    // set sample rate
+    tmp = AUD_OUT_SAMPLE_RATE;
+    if (ioctl(dev, SNDCTL_DSP_SPEED, &tmp) == -1) {
+        perror("Couldn't set sample rate (speed)");
+        *exit = 6;
+    }
+    if (tmp != AUD_OUT_SAMPLE_RATE) {
+        perror("This device doesn't support this sample rate");
+        *exit = 7;
+    }
 }
 
 Speaker::~Speaker() {
--- aud/speaker.hpp
+++ aud/speaker.hpp
@@ -1,6 +1,10 @@
 #ifndef AUD_SPEAKER_H
 #define AUD_SPEAKER_H
 
+#define AUD_OUT_FORMAT AFMT_S16_NE
+#define AUD_OUT_CHANNELS 1
+#define AUD_OUT_SAMPLE_RATE 44100
+
 class Speaker {
 public:
     explicit Speaker(int *exit);
